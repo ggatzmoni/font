d59ec1099c83ca79c98a3b70f70bc10d9df5ee18
ggatzmoni <68113737+ggatzmoni@users.noreply.github.com>
2021-02-01 15:17:02 +0100

    Add files via upload

--- font.py
+++ font.py
@@ -158,24 +158,12 @@ def initialize_model():
     # This is the first convolution
     tensorflow.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=(100, 100,3)),
     tensorflow.keras.layers.MaxPooling2D(2, 2),
-    # The second convolution
-#     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D(2,2),
-    # The third convolution
     tensorflow.keras.layers.Conv2D(128, (3,3), padding='same', activation='relu'),
     tensorflow.keras.layers.MaxPooling2D(2,2),
-    # The fourth convolution
-#     tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D(2,2),
     tensorflow.keras.layers.Conv2D(256, (3,3), padding='same', activation='relu'),
     tensorflow.keras.layers.MaxPooling2D(2,2),
-    # The fifth convolution
-#     tf.keras.layers.Conv2D(256, (3,3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D(2,2),
-    # Flatten the results to feed into a DNN
     tensorflow.keras.layers.Flatten(),
     tensorflow.keras.layers.Dropout(0.5),
-    # 512 neuron hidden layer
     tensorflow.keras.layers.Dense(512, activation='relu'),
     tensorflow.keras.layers.Dense(3, activation='softmax')
 ])
